36cd8b40ddc4c66f12f0bbc51985eaa5066191e2
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-16 20:45:23 +0530

    Update DecimalValues.java

--- DecimalValues.java
+++ DecimalValues.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
 
-public class PositiveNegativeNumbers {
+public class QuadraticEquation {
 
 	public static void main(String[] args) {
-		int Number;
-		Scanner s = new Scanner(System.in);
-		System.out.println("Input Any Number : ");
-		Number = s.nextInt();
 		
-		if (Number > 0) {
-			System.out.println("Positive Number");
-		}
-		else  if(Number < 0) {
-			System.out.println("Negative Number");
-		}
+		Scanner S = new Scanner(System.in);
+		
+		double a, b, c;
+		System.out.println("input value of a : ");
+		a = S.nextDouble();
+		System.out.println("input value of b : ");
+		b = S.nextDouble();
+		System.out.println("input value of c : ");
+		c = S.nextDouble();
+		
+		double formula = b * b - 4.0 * a *c;
+		
+		if (formula > 0.0){
+		double R1 = (-b + Math.pow(formula, 0.5))/(2.0 * a);
+		double R2 = (-b + Math.pow(formula, 0.5))/(2.0 * a);
+			System.out.println("The roots are " + R1 + " and " + R2);
+		  } 
+		
+		else if (formula == 0.0) {
+              double R1 = -b / (2.0 * a);
+              System.out.println("The root is " + R1);
+          }
+		
 		else {
-			System.out.println("zero");
-		}
-		
-	}
+              System.out.println("The equation has no real roots.");
+          }
 
-}
+		}
+	}
