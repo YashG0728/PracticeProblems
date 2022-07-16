099d3d0a3f7db39958fab53937428bf44b4905cc
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-16 20:43:00 +0530

    Update DecimalValues.java

--- DecimalValues.java
+++ DecimalValues.java
@@ -1,29 +1,76 @@
 import java.util.Scanner;
 
-public class GreaterNumber {
+public class NumOfDays {
 
 	public static void main(String[] args) {
 		
-		int a , b, c;
-		Scanner S = new Scanner(System.in);
-		System.out.println("Input a : ");
-		a = S.nextInt();
-		System.out.println("Input b : ");
-		b = S.nextInt();
-		System.out.println("Input c : ");
-		c = S.nextInt();
+		Scanner N = new Scanner(System.in);
+		
+		System.out.println("Enter Month: ");
+		int Month = N.nextInt();
+		
+		System.out.println("Enter Year: ");
+		int Year = N.nextInt();
+		String MonthOfName = "Unknown";
+		int NumOfDaysInMonth = 0;
+		
+		switch (Month) {
+		case 1: MonthOfName = "January";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 2: MonthOfName = "February";
+		if ((Year % 400 == 0) || ((Year % 4 == 0) && (Year % 100 != 0))) {
+			NumOfDaysInMonth = 29;
+		} else {
+			NumOfDaysInMonth = 28;
+		}
+		break;
+		
+		case 3: MonthOfName = "March";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 4: MonthOfName = "April";
+		NumOfDaysInMonth = 30;
+		break;
+		
+		case 5: MonthOfName = "May";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 6: MonthOfName = "June";
+		NumOfDaysInMonth = 30;
+		break;
+		
+		case 7: MonthOfName = "July";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 8: MonthOfName = "August";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 9: MonthOfName = "September";
+		NumOfDaysInMonth = 30;
+		break;
+		
+		case 10: MonthOfName = "Octomber";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		case 11: MonthOfName = "November";
+		NumOfDaysInMonth = 30;
+		break;
+		
+		case 12: MonthOfName = "December";
+		NumOfDaysInMonth = 31;
+		break;
+		
+		}
+		
+		System.out.print(MonthOfName + " " + Year + " has " + NumOfDaysInMonth + " days");
 
-		if (a > b)
-		if (a > c) {
-			System.out.println("A is greater ");
-		}	 
-		if (b > c)
-		if (b > a) {
-			System.out.println("B is greater");
-		}
-		if (c > a) 
-		if (c > b) {
-	    	System.out.println("C is greater");
-		}
 	}
+
 }
