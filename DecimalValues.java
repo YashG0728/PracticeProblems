feaf428cc26586005621b2e0e58b6aef507ff0a5
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-16 20:46:15 +0530

    Update DecimalValues.java

--- DecimalValues.java
+++ DecimalValues.java
@@ -1,35 +1,45 @@
 import java.util.Scanner;
 
-public class QuadraticEquation {
+public class SmallLargeNumber {
 
 	public static void main(String[] args) {
 		
-		Scanner S = new Scanner(System.in);
-		
-		double a, b, c;
-		System.out.println("input value of a : ");
-		a = S.nextDouble();
-		System.out.println("input value of b : ");
-		b = S.nextDouble();
-		System.out.println("input value of c : ");
-		c = S.nextDouble();
-		
-		double formula = b * b - 4.0 * a *c;
-		
-		if (formula > 0.0){
-		double R1 = (-b + Math.pow(formula, 0.5))/(2.0 * a);
-		double R2 = (-b + Math.pow(formula, 0.5))/(2.0 * a);
-			System.out.println("The roots are " + R1 + " and " + R2);
-		  } 
-		
-		else if (formula == 0.0) {
-              double R1 = -b / (2.0 * a);
-              System.out.println("The root is " + R1);
-          }
-		
-		else {
-              System.out.println("The equation has no real roots.");
-          }
-
+	double input;
+	Scanner S = new Scanner(System.in);
+	System.out.println("input value : ");
+	input = S.nextDouble();
+	
+	if (input > 0)
+	{
+	if (input < 1) {
+	System.out.println("Positive Small Number");
+	}
+	else if(input > 1000000) {
+		System.out.println("Positive large number");
+	}
+	else {
+             System.out.println("Positive number");
+         }	
+	}
+	else if (input < 0)
+    {
+        if (Math.abs(input) < 1)
+        {
+            System.out.println("Negative small number");
+        }
+        else if (Math.abs(input) > 1000000)
+        {
+            System.out.println("Negative large number");
+        }
+        else
+        {
+            System.out.println("Negative number");
+        }
+    }
+    else
+    {
+        System.out.println("Zero");
+	
 		}
+	  }
 	}
