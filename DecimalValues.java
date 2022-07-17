23342c9202a1a675659dd00190945501ef2e9b40
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-17 20:08:09 +0530

    Update DecimalValues.java

--- DecimalValues.java
+++ DecimalValues.java
@@ -1,28 +1,30 @@
-public class ConstructorOne {
-	public String language;
+//Create a package named ' EncapsalationTest' and make a class 'Number'. 
+package EncapsalationTest;
+public class number  
+{
+	//Declare field/variable as private. 
+	private int y;
 	
-	//constructor
-	ConstructorOne() {
-		this.language = "Java";
-		}
-	
-	ConstructorOne(String language) {
-		this.language = language;
-	}
-	
-	void getname() {
-	System.out.println("Programming language: " +this.language);	
-	}
-	
-	
-	public static void main(String[] args) {
-		ConstructorOne S1 = new ConstructorOne();
-		ConstructorOne S2 = new ConstructorOne("Python");
-		ConstructorOne S3 = new ConstructorOne("rython");
-		S1.getname();
-		S2.getname();
-		S3.getname();
-	}
-
+	//Create a public getter method for private variable. 
+	public int get() {
+	return y;
 }
-
+	//Create a public setter method for private variable and declare the parameter. 
+	public void set(int y) {
+	this.y=y;
+}
+	public class encaptest 
+	{
+		public static void main(String[] args)
+	{
+			//create a  object of class using new keyword
+			number S = new number();
+			//call setter method
+			S.set(5);
+			//call getter method
+			int num = S.get();
+			
+			System.out.println(num);
+	}
+	}
+}
