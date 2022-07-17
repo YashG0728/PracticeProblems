b25098edf949d7c62fdd9906a9b686524e6d5ec6
yash <107634548+YashG0728@users.noreply.github.com>
2022-07-17 19:37:37 +0530

    Update DecimalValues.java

--- DecimalValues.java
+++ DecimalValues.java
@@ -1,26 +1,28 @@
-import java.util.Scanner;
-
-public class DecimalValues {
-
+public class ConstructorOne {
+	public String language;
+	
+	//constructor
+	ConstructorOne() {
+		this.language = "Java";
+		}
+	
+	ConstructorOne(String language) {
+		this.language = language;
+	}
+	
+	void getname() {
+	System.out.println("Programming language: " +this.language);	
+	}
+	
+	
 	public static void main(String[] args) {
-		
-	Scanner S = new Scanner(System.in);
+		ConstructorOne S1 = new ConstructorOne();
+		ConstructorOne S2 = new ConstructorOne("Python");
+		ConstructorOne S3 = new ConstructorOne("rython");
+		S1.getname();
+		S2.getname();
+		S3.getname();
+	}
 
-	    System.out.print("Input floating-point number: ");
-	    double x = S.nextDouble();
-	    System.out.print("Input floating-point another number: ");
-	    double y = S.nextDouble();
-	      x = Math.round(x * 1000);
-	      y = Math.round(y * 1000);
-	      
-	    if (x == y)
-	    {
-	        System.out.println("They are the same up to three decimal places");
-	    }
-	    else
-	    {
-	        System.out.println("They are different");
-	     }
-	 }	
 }
 
